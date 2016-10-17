275e57c06bae19f5bb83b9f3bce9b8deeb589e25
raimundo.filho <raimundo.filho@LAB1-DCX-40.dcx.ufpb.br>
2016-10-17 09:42:37 -0300

    Adicionando variáveis e o método soma...

--- src/firstPackage/ClasseWendellSoares.java
+++ src/firstPackage/ClasseWendellSoares.java
@@ -3,5 +3,12 @@ package firstPackage;
 public class ClasseWendellSoares {
 	
 	private int a=2;
+	private int b=5;
+	private int c=8;
+	
+	public int soma(int a, int b, int c){
+		int soma = a + b+ c;
+		return soma;
+	}
 
 }
